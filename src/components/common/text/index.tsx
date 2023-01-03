7ead245b1438d7b191c5290380a472d52a27447d
jisoung <jangjisoung0405@gmail.com>
2023-01-03 09:46:27 +0900

    Fix :  파일 디렉토리 관련 오류 수정

--- src/components/common/text/index.tsx
+++ src/components/common/text/index.tsx
@@ -2,28 +2,31 @@ import styled from '@emotion/styled';
 import { ReactNode } from 'react';
 import { colorKeyOfType } from '../../../styles/theme/color';
 import { fontKeyOfType } from '../../../styles/theme/font';
-import { marginCssType, marginToCss } from '../../../utils/margin.tsx';
+import { marginCssType, marginToCss } from '../../../utils/margin';
+
+type TDisplay = 'inline' | 'inline-block' | 'block';
+type TAlign = 'center' | 'start' | 'end';
 
 interface PropsType extends marginCssType {
   className?: string;
-  display?: 'inline' | 'inline-block' | 'block';
+  display?: TDisplay;
   size?: fontKeyOfType;
   /** 타입 지정 필요 */
   color?: colorKeyOfType;
   children?: ReactNode;
   onClick?: any;
   cursor?: 'pointer';
-  align?: 'center' | 'start' | 'end';
+  align?: TAlign;
 }
 
-export const Text = ({ ...args }: PropsType) => (
-  <div {...args}>{args.children}</div>
+export const Text = ({ ...props }: PropsType) => (
+  <_Text {...props}>{props.children}</_Text>
 );
 
 const _Text = styled.div<PropsType>`
   display: ${({ display }) => display};
-  color: ${({ color, theme }) => theme.color[color]};
-  ${({ size, theme }) => theme.font[size]};
+  color: ${({ color, theme }) => theme.color[color!]};
+  ${({ size, theme }) => theme.font[size!]};
   ${({ margin }) => marginToCss({ margin })};
   cursor: ${({ cursor }) => cursor};
   text-align: ${({ align }) => align};
