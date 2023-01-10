5a775114cd2f78e23b8a65f29cb039c252cd7590
jisoung <jangjisoung0405@gmail.com>
2023-01-10 16:48:56 +0900

    Feat : global font 적용

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,6 +10,13 @@ export const global = css`
     outline: unset;
     list-style: none;
     font-style: normal;
+    @font-face {
+      font-family: 'Pretendard-Regular';
+      src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+        format('woff');
+      font-weight: 400;
+      font-style: normal;
+    }
   }
 
   button,
